56670abc99db67513c04786307f0136a9c925aef
Guoliang Hua <guohua@microsoft.com>
2019-03-29 21:58:14 +0800

    revert change to custom_op_via_python.py, fix custom_op_handler issue

--- examples/custom_op_via_python.py
+++ examples/custom_op_via_python.py
@@ -4,7 +4,8 @@ A simple example how to map a custom op in python.
 import tensorflow as tf
 import tf2onnx
 from onnx import helper
-from tf2onnx import constants
+
+_TENSORFLOW_DOMAIN = "ai.onnx.converters.tensorflow"
 
 
 def print_handler(ctx, node, name, args):
@@ -13,7 +14,7 @@ def print_handler(ctx, node, name, args):
     # becomes:
     #   T output = Identity(T Input)
     node.type = "Identity"
-    node.domain = constants.DEFAULT_CUSTOM_OP_OPSET.domain
+    node.domain = _TENSORFLOW_DOMAIN
     del node.input[1:]
     return node
 
@@ -24,8 +25,8 @@ with tf.Session() as sess:
     x_ = tf.Print(x, [x], "hello")
     _ = tf.identity(x_, name="output")
     onnx_graph = tf2onnx.tfonnx.process_tf_graph(sess.graph,
-                                                 custom_op_handlers={"Print": print_handler},
-                                                 extra_opset=[constants.DEFAULT_CUSTOM_OP_OPSET],
+                                                 custom_op_handlers={"Print": (print_handler, [])},
+                                                 extra_opset=[helper.make_opsetid(_TENSORFLOW_DOMAIN, 1)],
                                                  input_names=["input:0"],
                                                  output_names=["output:0"])
     model_proto = onnx_graph.make_model("test")
