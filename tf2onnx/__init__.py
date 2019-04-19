2832b45a810266d5404f8f54e9d687b1f845ddd9
Guoliang Hua <guohua@microsoft.com>
2019-04-19 21:05:17 +0800

    fix shadow import

--- tf2onnx/__init__.py
+++ tf2onnx/__init__.py
@@ -9,5 +9,5 @@ from __future__ import unicode_literals
 __all__ = ["utils", "graph_matcher", "graph", "loader", "tfonnx", "shape_inference", "schemas"]
 
 from .version import version as __version__
-from . import logging
+from . import verbose_logging as logging
 from tf2onnx import tfonnx, utils, graph, graph_matcher, shape_inference, schemas  # pylint: disable=wrong-import-order
