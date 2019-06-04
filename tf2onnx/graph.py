b51ff912730e055e8ee75468d6487aac2019ea3f
Yuanhong Wang <lucienwang@qq.com>
2019-06-04 01:42:35 +0000

    check opset version

--- tf2onnx/graph.py
+++ tf2onnx/graph.py
@@ -868,8 +868,11 @@ class Graph(object):
 
         # create input_tensor_values
         input_ids = [op.output[0] for op in placeholder_ops]
-        # onnx below IR 4 requires initializer should be in inputs
-        if onnx.IR_VERSION < 4:
+        # onnx with IR version below 4 requires initializer should be in inputs.
+        # here we check opset version rather than IR version for the reason:
+        # https://github.com/onnx/tensorflow-onnx/pull/557
+        # opset 9 come with IR 4.
+        if self.opset < 9:
             input_ids += [op.output[0] for op in const_ops]
 
         input_tensor_values = self.make_onnx_graph_io(input_ids)
