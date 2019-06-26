f9ec94d7b7f9ce81a454a16c6dce442a5b93a04f
t-leqiao <t-leqiao@microsoft.com>
2019-06-26 15:59:31 +0800

    replace node.get_attr('xxx').i by node.get_attr_value('xxx')

--- tf2onnx/onnx_opset/tensor.py
+++ tf2onnx/onnx_opset/tensor.py
@@ -1257,7 +1257,7 @@ class ReverseSequence:
         seq_dim = node.get_attr("seq_dim")
         utils.make_sure(seq_dim is not None, "sequence dim must be given in {}".format(node.name))
         seq_dim = seq_dim.i
-        batch_dim = node.get_attr_value("batch_dim")
+        batch_dim = node.get_attr_value("batch_dim", 0)
 
         ctx.remove_node(node.name)
         node = ctx.make_node(
