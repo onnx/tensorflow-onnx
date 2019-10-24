653b0551586643a4f4a0fc2f8c896c82bd58c91a
Randy Shuai <rashuai@microsoft.com>
2019-10-24 14:43:45 -0700

    set attributes for resize opset11

--- tf2onnx/onnx_opset/nn.py
+++ tf2onnx/onnx_opset/nn.py
@@ -609,7 +609,7 @@ class Resize:
         if roi_required:
             roi = ctx.make_const(utils.make_name("roi"), np.array([]).astype(np.float32))
             upsample = ctx.make_node("Resize", [input_nchw.output[0], roi.output[0], scales.output[0]],
-                                     attr={"mode": mode})
+                                     attr={"mode": mode, "nearest_mode": "floor", "coordinate_transformation_mode": "asymmetric"})
         else:
             upsample = ctx.make_node(op_type, [input_nchw.output[0], scales.output[0]], attr={"mode": mode})
 
