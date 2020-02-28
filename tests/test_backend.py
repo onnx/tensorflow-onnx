9329eff5c99e8dfcb8e800ee767cb8da7562d930
Guenther Schmuelling <guschmue@microsoft.com>
2020-02-27 17:02:13 -0800

    merge master

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -2899,23 +2899,18 @@ class BackendTests(Tf2OnnxBackendTestBase):
 
     @check_opset_min_version(11)
     def test_matrix_diag_part(self):
-        input_val = np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]]).astype(
-            np.int64)
-        input_holder = tf.placeholder(tf.int64, input_val.shape, name=_TFINPUT)
-        _ = tf.matrix_diag_part(input_holder, name=_TFOUTPUT)
-        self._run_test_case([_OUTPUT], {_INPUT: input_val})
-        tf.reset_default_graph()
-        input_val = np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]]).astype(
-            np.int64)
-        input_holder = tf.placeholder(tf.int64, input_val.shape, name=_TFINPUT)
-        _ = tf.matrix_diag_part(input_holder, name=_TFOUTPUT)
-        self._run_test_case([_OUTPUT], {_INPUT: input_val})
-        tf.reset_default_graph()
-        input_val = np.array([[[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]],
-                              [[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]]]).astype(np.int64)
-        input_holder = tf.placeholder(tf.int64, input_val.shape, name=_TFINPUT)
-        _ = tf.matrix_diag_part(input_holder, name=_TFOUTPUT)
-        self._run_test_case([_OUTPUT], {_INPUT: input_val})
+        input_vals = [
+            np.array([[[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]]], dtype=np.int64),
+            np.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]], dtype=np.int64),
+            np.array([[[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]],
+                      [[[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12]]]], dtype=np.int64)]
+
+        def func(input_holder):
+            return tf.compat.v1.matrix_diag_part(input_holder, name=_TFOUTPUT)
+
+        for input_val in input_vals:
+            self._run_test_case(func, [_OUTPUT], {_INPUT: input_val})
+
 
 if __name__ == '__main__':
     unittest_main()
