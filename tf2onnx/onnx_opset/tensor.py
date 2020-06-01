5cc8f27d2ac08087416bf1f4fc6502305ffff992
Jignesh Parmar <jignparm@microsoft.com>
2020-06-01 21:07:25 +0000

    Add support for Keras resnet50 pretrained model testing.

--- tf2onnx/onnx_opset/tensor.py
+++ tf2onnx/onnx_opset/tensor.py
@@ -1868,7 +1868,7 @@ class MatrixDiagPartV2V3:
         const_xlenp = mkconst(np.array([xlenp], np.int64), 'const_xlenp')
         const_stride = mkconst(np.array([xlenp + 1], np.int64), 'const_stride')
         const_minxy = mkconst(np.array([min(xlen, ylen)], np.int64), 'const_minxy')
-        const_minxy_float = mkconst(np.array([min(xlen, ylen)], np.float32), 'const_minxy')
+        const_minxy_float = mkconst(np.array([min(xlen, ylen)], np.float32), 'const_minxy_f')
         const_xmax = mkconst(np.array([xlen * xlenp + xlenp - 1], np.int64), 'const_xmax')
         const_ymax = mkconst(np.array([xlenp * ylen - 1], np.int64), 'const_ymax')
         const_ymax_float = mkconst(np.array([xlenp * ylen - 1], np.float32), 'const_ymax_f')
