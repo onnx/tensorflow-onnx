919babf39211481aa27e1833eaa19db529749b03
phager90 <hagerpascalalexander@msn.com>
2020-08-07 13:01:47 +0200

    Apply suggestions from code review
    Co-authored-by: TomWildenhain-Microsoft <67606533+TomWildenhain-Microsoft@users.noreply.github.com>

--- tf2onnx/optimizer/const_fold_optimizer.py
+++ tf2onnx/optimizer/const_fold_optimizer.py
@@ -113,7 +113,7 @@ class ConstFoldOptimizer(GraphOptimizerBase):
     @staticmethod
     @_register_func("Reshape")
     def _fold_reshape(node, graph):
-        const_val_data = node.inputs[0].get_tensor_value(as_list=False) 
+        const_val_data = node.inputs[0].get_tensor_value(as_list=False)
         const_val_shape = node.inputs[1].get_tensor_value(as_list=False)
         const_val_after_trans = const_val_data.reshape(const_val_shape)
         return [const_val_after_trans]
