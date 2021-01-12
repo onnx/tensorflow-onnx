743f6fc875bb271ce1936547381ad4d396aa565b
Tom Wildenhain <tomwi@microsoft.com>
2021-01-12 15:15:39 -0500

    Added handlers for tflite
    Signed-off-by: Tom Wildenhain <tomwi@microsoft.com>

---
+++ tf2onnx/tflite_handlers/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT license.
+"""tf2onnx.tflite_handlers module"""
+
+from . import (
+    tfl_math,
+    tfl_nn,
+    tfl_controlflow,
+    tfl_direct,
+    tfl_tensor
+)
