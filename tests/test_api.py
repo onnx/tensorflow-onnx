f7c281a917f9ad3a75165a899030c695747f10c5
Guenther Schmuelling <guschmue@microsoft.com>
2021-02-22 18:01:51 -0800

    merge master
    Signed-off-by: Guenther Schmuelling <guschmue@microsoft.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -30,10 +30,12 @@ class ApiTests(Tf2OnnxBackendTestBase):
     def create_model(self):
         strides = 2
         inp = tf.keras.Input(shape=[None, None, 3], name="input")
+        n = tf.keras.Input(shape=(), name="n")
         conv = tf.keras.layers.Conv2D(
             filters=64, kernel_size=7, strides=strides, name="conv1")(inp)
         conv = tf.keras.layers.BatchNormalization()(conv)
-        model = tf.keras.Model(inputs=inp, outputs=conv,
+        conv = conv * n
+        model = tf.keras.Model(inputs=[inp, n], outputs=conv,
                                name="test_model", trainable=True)
         optimizer = tf.keras.optimizers.Adam(1e-4, 1e-8)
         model.compile(optimizer=optimizer, loss="mean_squared_error")
@@ -43,8 +45,10 @@ class ApiTests(Tf2OnnxBackendTestBase):
         model = self.create_model()
         shape = [1, 224, 224, 3]
         x = np.arange(np.prod(shape)).reshape(shape).astype(np.float32)
-        ky = model(x)
-        spec = (tf.TensorSpec((None, 224, 224, 3), tf.float32, name="input"),)
+        n = np.array([2.], dtype=np.float32)
+        ky = model.predict([x, n])
+        spec = (tf.TensorSpec((None, 224, 224, 3), tf.float32, name="input"),
+                tf.TensorSpec((), tf.float32, name="n"))
         if large_model:
             output_path = os.path.join(self.test_data_directory, "model.zip")
         else:
@@ -60,11 +64,11 @@ class ApiTests(Tf2OnnxBackendTestBase):
                 z.extractall(os.path.dirname(output_path))
             output_path = os.path.join(os.path.dirname(output_path), "__MODEL_PROTO.onnx")
 
-        oy = self.run_onnxruntime(output_path, {"input:0": x}, output_names)
-        self.assertAllClose(ky.numpy(), oy[0], rtol=0.3, atol=0.1)
+        oy = self.run_onnxruntime(output_path, {"input": x, "n": n}, output_names)
+        self.assertAllClose(ky, oy[0], rtol=0.3, atol=0.1)
         # make sure the original keras model wasn't trashed
-        ky1 = model(x)
-        self.assertAllClose(ky1.numpy(), oy[0], rtol=0.3, atol=0.1)
+        ky1 = model.predict([x, n])
+        self.assertAllClose(ky1, oy[0], rtol=0.3, atol=0.1)
 
     @check_tf_min_version("2.0")
     def test_keras_api(self):
@@ -93,11 +97,11 @@ class ApiTests(Tf2OnnxBackendTestBase):
         model_proto, _ = tf2onnx.convert.from_function(concrete_func, input_signature=spec,
                                                        opset=self.config.opset, output_path=output_path)
         output_names = [n.name for n in model_proto.graph.output]
-        oy = self.run_onnxruntime(output_path, {"x:0": x, "y:0": y}, output_names)
+        oy = self.run_onnxruntime(output_path, {"x": x, "y": y}, output_names)
         self.assertAllClose(ky, oy[0], rtol=0.3, atol=0.1)
 
     @check_tf_min_version("1.15")
-    def test_graphdef(self):
+    def _test_graphdef(self):
         def func(x, y):
             return x * y
 
@@ -119,6 +123,23 @@ class ApiTests(Tf2OnnxBackendTestBase):
         output_names = [n.name for n in model_proto.graph.output]
         oy = self.run_onnxruntime(output_path, {"x:0": x, "y:0": y}, output_names)
         self.assertAllClose(ky, oy[0], rtol=0.3, atol=0.1)
+
+
+    @check_tf_min_version("1.15")
+    def test_graphdef(self):
+        output_path = os.path.join(self.test_data_directory, "model.onnx")
+        graph_def, input_names, outputs = tf2onnx.tf_loader.from_graphdef(
+            "tests/models/regression/graphdef/frozen.pb", ["X:0"], ["pred:0"])
+
+        x = np.array([5.], dtype=np.float32)
+        tensors_to_rename = {"pred:0": "pred", "X:0": "X"}
+        model_proto, _ = tf2onnx.convert.from_graph(graph_def, input_names=["X:0"], output_names=["pred:0"],
+                                                    opset=self.config.opset, output_path=output_path,
+                                                    tensors_to_rename=tensors_to_rename)
+        output_names = [n.name for n in model_proto.graph.output]
+        oy = self.run_onnxruntime(output_path, {"X": x}, output_names)
+        self.assertTrue(output_names[0] == "pred")
+        self.assertAllClose([2.1193342], oy[0], rtol=0.1, atol=0.1)
 
 
 if __name__ == '__main__':
