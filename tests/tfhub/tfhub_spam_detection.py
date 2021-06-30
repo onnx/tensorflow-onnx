db230dc5758100e6064cb6547dd41fd341177198
xavier dupré <xavier.dupre@gmail.com>
2021-06-30 13:09:39 +0200

    refactoring
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/tfhub/tfhub_spam_detection.py
+++ tests/tfhub/tfhub_spam_detection.py
@@ -2,14 +2,18 @@
 import os
 import random
 import numpy
-from _tools import generate_random_images, measure_time, download_model, convert_model, benchmark
+from _tools import generate_random_images, benchmark
 
-url = "https://tfhub.dev/tensorflow/tutorials/spam-detection/1?tf-hub-format=compressed"
-dest = "tf-spam-detection"
-name = "spam-detection"
-opset = 13
-onnx_name = os.path.join(dest, "%s-%d.onnx" % (name, opset))
 
-imgs = generate_random_images((1, 20), dtype=numpy.int32)
+def main(opset=13):
+    url = "https://tfhub.dev/tensorflow/tutorials/spam-detection/1?tf-hub-format=compressed"
+    dest = "tf-spam-detection"
+    name = "spam-detection"
+    onnx_name = os.path.join(dest, "%s-%d.onnx" % (name, opset))
 
-benchmark(url, dest, onnx_name, opset, imgs)
+    imgs = generate_random_images((1, 20), dtype=numpy.int32)
+
+    benchmark(url, dest, onnx_name, opset, imgs)
+
+
+main()
