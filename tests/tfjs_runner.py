cccca6a431fc710b901a8b9af9b35ec345973e21
Tom Wildenhain <tomwi@microsoft.com>
2021-07-13 21:17:50 -0700

    reorganize
    Signed-off-by: Tom Wildenhain <tomwi@microsoft.com>

--- tests/tfjs_runner.py
+++ tests/tfjs_runner.py
@@ -1,11 +1,18 @@
+# SPDX-License-Identifier: Apache-2.0
+
+
+"""Contains tools for running tfjs models. Requires nodejs."""
+
 import os
-import sys
 import subprocess
-import numpy as np
 import json
 import base64
 
+import numpy as np
+
+
 def numpy_to_json(np_arr):
+    """Encodes a numpy array to a json-serializable dict"""
     # TFJS only has types float32, int32, bool, string and complex64
     dtype_map = {
         'b': 'bool',
@@ -25,10 +32,13 @@ def numpy_to_json(np_arr):
     if dtype == 'string':
         result['data'] = np_arr.flatten().tolist()
     else:
+        # This is faster for JSON to parse and can represent inf/nan values
         result['dataEnc'] = base64.encodebytes(np_arr.astype(dtype).tobytes()).decode()
     return result
 
+
 def json_to_numpy(obj):
+    """Decodes a dict from json into a numpy array"""
     dtype = obj['dtype']
     if dtype == 'string':
         dtype = 'str'
@@ -37,35 +47,25 @@ def json_to_numpy(obj):
     data_bytes = base64.decodebytes(obj['dataEnc'].encode())
     return np.frombuffer(data_bytes, dtype).reshape(obj['shape'])
 
+
 def input_to_json(input):
+    """Encodes a dict, list or single numpy array into a corresponding json representation understood by run_tfjs.js"""
     if isinstance(input, list):
         return [numpy_to_json(arr) for arr in input]
     if isinstance(input, dict):
         return {k: numpy_to_json(v) for k, v in input.items()}
     return numpy_to_json(input)
 
+
 def json_to_output(obj):
+    """Given an object representing an output from run_tfjs.js, returns a list of numpy arrays (representing the
+    output from the model). NOTE: the output of this function is always a list of arrays."""
     if isinstance(obj, list):
         return [json_to_numpy(x) for x in obj]
     if all(isinstance(x, dict) for x in obj.values()):
-        return {k: json_to_numpy(v) for k, v in obj.itms()}
+        return [json_to_numpy(v) for v in obj.values()]
     return [json_to_numpy(obj)]
 
-def test():
-    float_array = np.array([[1.1, 2.2], [3.3, 4.4]], np.float32)
-    int_array = np.array([[1, 2], [3, 4]], np.int32)
-    bool_array = np.array([[True, False], [True, True]], np.bool)
-    string_array = np.array([['Hello world', ''], ['test', 'Tensor']], np.str)
-    complex_array = np.array([[1 + 0.1j, 2 + 0.2j], [3 + 0.3j, 4 + 0.4j]], np.complex64)
-
-    arrays = [float_array, int_array, bool_array, string_array, complex_array]
-    for arr in arrays:
-        array_enc = numpy_to_json(arr)
-        print(array_enc)
-        array_dec = json_to_numpy(array_enc)
-        np.testing.assert_equal(arr, array_dec)
-
-    print("Tests pass")
 
 def run_tfjs(tfjs_path, inputs):
     script_path = os.path.join(os.path.dirname(__file__), 'run_tfjs.js')
