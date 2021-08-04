6319f87e6c712a4caed4886133519f510a5cf307
xavier dupré <xavier.dupre@gmail.com>
2021-08-04 19:58:42 +0200

    update script to test tfhub models
    Signed-off-by: xavier dupré <xavier.dupre@gmail.com>

--- tests/tfhub/tfhub_yamnet_tf.py
+++ tests/tfhub/tfhub_yamnet_tf.py
@@ -1,7 +1,7 @@
 # SPDX-License-Identifier: Apache-2.0
 import os
 import numpy
-from _tools import generate_random_images, benchmark
+from _tools import generate_random_images, benchmark, benchmark_tflite
 
 
 def main(opset=13):
@@ -9,10 +9,14 @@ def main(opset=13):
     dest = "tf-yamnet-tf"
     name = "yamnet"
     onnx_name = os.path.join(dest, "%s-%d.onnx" % (name, opset))
+    tfl = os.path.join(dest, 'model.tflite')
 
     imgs = generate_random_images(shape=(16000, ), dtype=numpy.float32, scale=0.)
 
-    benchmark(url, dest, onnx_name, opset, imgs)
+    # benchmark(url, dest, onnx_name, opset, imgs, convert_tflite=tfl)
+
+    onnx_name = os.path.join(dest, "%s-tfl-%d.onnx" % (name, opset))
+    benchmark_tflite(tfl, dest, onnx_name, opset, imgs)
 
 
 if __name__ == "__main__":
