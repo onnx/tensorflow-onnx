52ee1715c0ddc4592d266f3402aff94df840799d
Deyu Huang <deyhuang@microsoft.com>
2022-05-17 15:22:30 +0800

    add doc change
    Signed-off-by: Deyu Huang <deyhuang@microsoft.com>

--- setup.py
+++ setup.py
@@ -95,7 +95,6 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9']
