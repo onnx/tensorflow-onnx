be6e457b6170a1eb7b521f0adb4470ed1b5f0291
Deyu Huang <deyhuang@microsoft.com>
2022-05-25 14:45:27 +0800

    add TensorScatterAdd op conversion in opset 16
    Signed-off-by: Deyu Huang <deyhuang@microsoft.com>

--- tf2onnx/onnx_opset/tensor.py
+++ tf2onnx/onnx_opset/tensor.py
@@ -654,6 +654,14 @@ class ScatterND:
         # reorder inputs to match onnx
         ctx.replace_inputs(node, [node.input[2], node.input[0], node.input[1]])
 
+@tf_op("TensorScatterAdd", onnx_op="ScatterND")
+class ScatterND:
+    @classmethod
+    def version_16(cls, ctx, node, **kwargs):
+        # indicies input msut be int64 in ONNX.
+        if ctx.get_dtype(node.input[1]) != TensorProto.INT64:
+            ctx.insert_new_node_on_input(node, "Cast", node.input[1], to=TensorProto.INT64)
+        node.set_attr("reduction", 'add')
 
 @tf_op("TensorScatterUpdate", onnx_op="ScatterND")
 class TensorScatterUpdate:
