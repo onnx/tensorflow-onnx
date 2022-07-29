70ccb5512da9412a54cb7786f00b6d9bdd98e55d
Deyu Huang <deyhuang@microsoft.com>
2022-07-29 16:45:33 +0800

    Increment main to 1.12
    Signed-off-by: Deyu Huang <deyhuang@microsoft.com>

--- tf2onnx/version.py
+++ tf2onnx/version.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-version = '1.11.0'
-git_version = 'e9b6cb4fca61c87fcdaf4e9610535910c01ea4f9'
+version = '1.12.0'
+git_version = '087045d4b61e231897f1232de59609d30013b8f5'
