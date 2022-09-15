e04c52ba59c5571f694baabe25cc5ad02e5c862d
Jay Zhang <jiz@microsoft.com>
2022-09-15 11:36:47 +0800

    Update the way to verify if it is tf2.0.
    Signed-off-by: Jay Zhang <jiz@microsoft.com>

--- tests/keras2onnx_unit_tests/mock_keras2onnx/proto/tfcompat.py
+++ tests/keras2onnx_unit_tests/mock_keras2onnx/proto/tfcompat.py
@@ -3,10 +3,9 @@
 import os
 import tensorflow as _tf
 
-from distutils.version import StrictVersion
+from packaging.version import Version
 
-is_tf2 = StrictVersion(_tf.__version__.split('-')[0]) >= StrictVersion('2.0.0')
-
+is_tf2 = Version(_tf.__version__) >= Version("2.0")
 
 def normalize_tensor_shape(tensor_shape):
     if is_tf2:
