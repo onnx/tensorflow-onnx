21c68294ed0692258dc173b839ddcb2cf3b4b866
Jay Zhang <jiz@microsoft.com>
2022-09-21 11:21:27 +0800

    tf2onnx-1.12.1 release.
    Signed-off-by: Jay Zhang <jiz@microsoft.com>

--- tf2onnx/version.py
+++ tf2onnx/version.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-version = '1.12.0'
-git_version = '087045d4b61e231897f1232de59609d30013b8f5'
+version = '1.12.1'
+git_version = '6fdbe43458c7ed7ba285ce2dc3221c8a8c39bb4a'
