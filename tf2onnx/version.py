8e3d3ee3f2c9ed930e951b7ab576a76fa1a896d6
Jay Zhang <36183870+fatcat-z@users.noreply.github.com>
2022-11-04 15:55:19 +0800

    Prepare updates for release 1.13.0. (#2074)
    Signed-off-by: Jay Zhang <jiz@microsoft.com>
    
    Signed-off-by: Jay Zhang <jiz@microsoft.com>
    Signed-off-by: Salvetti, Francesco <Francesco.Salvetti@nuance.com>

--- tf2onnx/version.py
+++ tf2onnx/version.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-version = '1.12.0'
-git_version = '087045d4b61e231897f1232de59609d30013b8f5'
+version = '1.13.0'
+git_version = 'ddca3a5eb2d912f20fe7e0568dd1a3013aee9fa3'
