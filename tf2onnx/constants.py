1d4d5460e858e615cde6b9922167de4183468096
Jay Zhang <jiz@microsoft.com>
2023-03-27 12:11:38 +0800

    Change the default opset to 15.
    Signed-off-by: Jay Zhang <jiz@microsoft.com>

--- tf2onnx/constants.py
+++ tf2onnx/constants.py
@@ -18,8 +18,8 @@ CONTRIB_OPS_DOMAIN = "ai.onnx.contrib"
 # Default opset version for onnx domain.
 # The current update policy is that the default should be set to
 # the latest released version as of 18 months ago.
-# Opset 13 was released in ONNX v1.8.0 (Nov, 2020).
-PREFERRED_OPSET = 13
+# Opset 15 was released in ONNX v1.10.0 (Jul, 2021).
+PREFERRED_OPSET = 15
 
 # Default opset for custom ops
 TENSORFLOW_OPSET = helper.make_opsetid("ai.onnx.converters.tensorflow", 1)
