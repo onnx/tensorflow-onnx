c0d62f18c5ea78f5c2fb3dab1f7e478c7638912b
pagran <pagran@pm.me>
2023-05-28 16:26:43 +0300

    fix lint
    Signed-off-by: pagran <pagran@pm.me>

--- tf2onnx/custom_opsets/onnx_ml.py
+++ tf2onnx/custom_opsets/onnx_ml.py
@@ -42,7 +42,7 @@ class LookupTableFind:
 
         dtype = ctx.get_dtype(node.output[0])
         in_dtype = ctx.get_dtype(node.input[1])
-        utils.make_sure((dtype == TensorProto.INT64 and in_dtype == TensorProto.STRING) or 
+        utils.make_sure((dtype == TensorProto.INT64 and in_dtype == TensorProto.STRING) or
                         (dtype == TensorProto.STRING and in_dtype == TensorProto.INT64),
                         f"Only lookup tables of type string<->int64 are currently supported.")
 
