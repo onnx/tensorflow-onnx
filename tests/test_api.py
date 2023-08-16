4fc2231784e5c113752dab9a6f3056886fbc33de
Jay Zhang <jiz@microsoft.com>
2023-08-16 19:39:22 +0800

    Skip one test case.
    Signed-off-by: Jay Zhang <jiz@microsoft.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -12,7 +12,7 @@ import numpy as np
 import tensorflow as tf
 from onnx import helper
 
-from common import check_tf_min_version, unittest_main, requires_custom_ops, check_opset_min_version, skip_tf_versions
+from common import check_tf_min_version, check_tf_max_version, unittest_main, requires_custom_ops, check_opset_min_version, skip_tf_versions
 from tf2onnx.tf_loader import is_tf2
 from backend_test_base import Tf2OnnxBackendTestBase
 import tf2onnx
@@ -79,6 +79,7 @@ class ApiTests(Tf2OnnxBackendTestBase):
 
     @requires_custom_ops()
     @check_tf_min_version("1.15")
+    @check_tf_max_version("2.12")
     @check_opset_min_version(11, "SparseToDense")
     @skip_tf_versions(["2.1"], "TF 2.1 keras model doesn't work; table not initialized")
     def test_keras_hashtable(self):
