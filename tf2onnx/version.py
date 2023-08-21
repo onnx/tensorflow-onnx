20148a331788c1cc658d3aaccc803f8427d4e58b
Jay Zhang <jiz@microsoft.com>
2023-08-21 19:10:54 +0800

    Make changes for a new release.
    Signed-off-by: Jay Zhang <jiz@microsoft.com>

--- tf2onnx/version.py
+++ tf2onnx/version.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-version = '1.14.0'
-git_version = 'd071d17ca3f5c912ce4700d03c3f5254af70e5af'
+version = '1.15.0'
+git_version = 'ee37d58da2afabf12cb73dc8b30cc760106244cc'
