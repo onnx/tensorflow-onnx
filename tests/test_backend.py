b09ef84041b678a0af54c0f9db7939ac055e1ff7
Shuai Shao <shshao@nuro.ai>
2023-10-17 13:05:35 -0700

    Set min to tf2.0

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -695,7 +695,7 @@ class BackendTests(Tf2OnnxBackendTestBase):
             return tf.identity(conv, name=_TFOUTPUT)
         self._run_test_case(func, [_OUTPUT], {_INPUT: x_val, _INPUT1: x_val}, rtol=0.08)
 
-    @check_tf_min_version("2.11")
+    @check_tf_min_version("2.0")
     @check_tf_max_version("2.12")
     def test_depthwise_conv3d(self):
         strides = [1, 1, 1, 1, 1]
