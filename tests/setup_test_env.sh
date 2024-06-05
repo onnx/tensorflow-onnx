b8306a4ea618a9126d26d22a9757ffcdb3129ef5
Jay Zhang <jiz@microsoft.com>
2024-06-05 09:59:16 +0000

    Refactor.
    Signed-off-by: Jay Zhang <jiz@microsoft.com>

--- tests/setup_test_env.sh
+++ tests/setup_test_env.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
 # # Check if the argument is provided
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <tensorflow_version>"
+    echo "Usage: $1 <onnxruntime_version>"
     exit 1
 fi
 
 # Assign the argument to a variable
 TF_VERSION=$1
+ORT_VERSION=$2
 
 echo "==== TensorFlow version: $TF_VERSION"
+echo "==== ONNXRuntime version: $ORT_VERSION"
 
 pip install pytest pytest-cov pytest-runner coverage graphviz requests pyyaml pillow pandas parameterized sympy coloredlogs flatbuffers timeout-decorator
 pip install onnx
-pip install onnxruntime==1.16.3
+pip install onnxruntime==$ORT_VERSION
 pip install numpy
 
 pip install onnxruntime-extensions
