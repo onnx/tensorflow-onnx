178e25236425ee7d28a3e4427e64058d9d7f21bb
xiaowuhu <xiaowuhu@microsoft.com>
2024-06-24 18:38:43 +0800

    Revert "Complete the CI tests."

--- tests/utils/setup_test_env.sh
+++ tests/utils/setup_test_env.sh
@@ -1,24 +1,22 @@
 #!/bin/bash
 
 # # Check if the argument is provided
-if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <tensorflow_version> <onnxruntime_version> <onnx_version>"
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <tensorflow_version> <onnxruntime_version>"
     exit 1
 fi
 
 # Assign the argument to a variable
 TF_VERSION=$1
 ORT_VERSION=$2
-ONNX_VERSION=$3
 
 echo "==== TensorFlow version: $TF_VERSION"
 echo "==== ONNXRuntime version: $ORT_VERSION"
-echo "==== ONNX version: $ONNX_VERSION"
 
 pip install pytest pytest-cov pytest-runner coverage graphviz requests pyyaml pillow pandas parameterized sympy coloredlogs flatbuffers timeout-decorator
-pip install onnx==$ONNX_VERSION
+pip install onnx
 pip install onnxruntime==$ORT_VERSION
-pip install "numpy<2"
+pip install numpy
 
 pip install onnxruntime-extensions
 pip install "tensorflow-text<=$TF_VERSION"
