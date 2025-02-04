8f9756c0e0abf7f332800ea5bd812b1c64ced8de
Chayim I. Kirshen <c@kirshen.com>
2025-02-04 11:46:57 +0200

    troves for python 3.12

--- setup.py
+++ setup.py
@@ -98,5 +98,8 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.12']
         'Programming Language :: Python :: 3.10']
 )
