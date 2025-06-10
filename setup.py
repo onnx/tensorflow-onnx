b651bd040260ad5023931424cfa97b9f40fd432d
xadupre <xadupre@microsoft.com>
2025-06-10 16:46:39 +0200

    Remove py3.8
    Signed-off-by: xadupre <xadupre@microsoft.com>

--- setup.py
+++ setup.py
@@ -95,8 +95,8 @@ setup(
         'Topic :: Software Development :: Libraries',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10']
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12']
 )
