12b8122d20c82f57ae0913079e5a0271d68d2fe0
Adraub <adraub@gmail.com>
2025-10-31 23:59:11 +0100

    Fix RuntimeWarning: 'tf2onnx.convert' found in sys.modules after import of package 'tf2onnx', but prior to execution of 'tf2onnx.convert'
    Signed-off-by: Adraub <adraub@gmail.com>
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- tf2onnx/__init__.py
+++ tf2onnx/__init__.py
@@ -8,4 +8,3 @@ __all__ = ["utils", "graph_matcher", "graph", "graph_builder",
 import onnx
 from .version import git_version, version as __version__
 from . import verbose_logging as logging
-from tf2onnx import tfonnx, utils, graph, graph_builder, graph_matcher, shape_inference, schemas, convert  # pylint: disable=wrong-import-order
