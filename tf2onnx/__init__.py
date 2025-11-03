e8c6beb9bd07fd65922af413eda1d43f69584013
Adraub <adraub@gmail.com>
2025-11-03 22:15:35 +0100

    Add lazy loading for submodules in __init__.py
    Fixes some UT missing some submodules after removing heavy import
    
    Signed-off-by: Adraub <adraub@gmail.com>
    Signed-off-by: Andreas Fehlner <fehlner@arcor.de>

--- tf2onnx/__init__.py
+++ tf2onnx/__init__.py
@@ -5,6 +5,25 @@
 __all__ = ["utils", "graph_matcher", "graph", "graph_builder",
            "tfonnx", "shape_inference", "schemas", "tf_utils", "tf_loader", "convert"]
 
+import importlib
 import onnx
 from .version import git_version, version as __version__
 from . import verbose_logging as logging
+
+# NOTE: Importing heavily submodules here leads to a RuntimeWarning
+#    when launching "python -m tf2onnx.convert":
+#   "'tf2onnx.convert' found in sys.modules after import of package 'tf2onnx',
+#   but prior to execution of 'tf2onnx.convert'; this may result in unpredictable behaviour"
+# To prevent that, lazily import submodules on attribute access using PEP 562.
+
+def __getattr__(name):
+    """Lazily import submodules listed in __all__ on attribute access."""
+    if name in __all__:
+        module = importlib.import_module(f"tf2onnx.{name}")
+        globals()[name] = module
+        return module
+    raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
+
+def __dir__():
+    """Expose package attributes and available submodules for tab-completion."""
+    return sorted(list(globals().keys()) + __all__)
