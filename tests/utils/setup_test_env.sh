61ddecd8c42525c749741dd87490a2f7b314ce73
Tristan Lippold <tristan.lippold@metamorphosis.tech>
2026-02-18 12:26:59 +0100

    Fix workflow
    Signed-off-by: Tristan Lippold <tristan.lippold@metamorphosis.tech>

--- tests/utils/setup_test_env.sh
+++ tests/utils/setup_test_env.sh
@@ -25,8 +25,7 @@ pip install "tensorflow-text<=$TF_VERSION"
 
 pip uninstall -y tensorflow
 pip install tensorflow==$TF_VERSION
-pip uninstall -y protobuf
-pip install "protobuf~=3.20"
+pip install protobuf
 
 python setup.py install
 
